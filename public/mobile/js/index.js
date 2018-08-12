b18f48a8377bc71c54c0351531c3a20b02a48a34
liuziyi <552019419@qq.com>
2018-08-12 11:28:47 +0800

    更新

--- public/mobile/js/index.js
+++ public/mobile/js/index.js
@@ -1,13 +1,10 @@
 $(function () {
-  //开启轮播
-  //获得slider插件对象
-  var gallery = mui('.mui-slider');
-  gallery.slider({
-    interval: 500//自动轮播周期，若为0则不自动播放，默认为0；
-  });
-  // 滚动区域
-  mui('.mui-scroll-wrapper').scroll({
-    deceleration: 0.0005, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
-    indicators: true, //是否显示滚动条
-  });
-})
+    /*初始化轮播图*/
+    mui('.mui-slider').slider({
+        interval:4000
+    });
+    /*初始化区域滚动*/
+    mui('.mui-scroll-wrapper').scroll({
+        indicators:false
+    });
+});
